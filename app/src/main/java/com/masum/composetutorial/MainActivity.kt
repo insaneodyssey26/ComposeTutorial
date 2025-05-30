2debb4d78d0d336c6196e97a9cb933deea469d63
Masum <hydrapikachu29@gmail.com>
2025-05-31 01:37:55 +0530

    Justified text aligns

--- app/src/main/java/com/masum/composetutorial/MainActivity.kt
+++ app/src/main/java/com/masum/composetutorial/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -59,10 +60,12 @@ fun Article(
         Text(
             text = subheading,
             modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
         )
         Text(
             text = content,
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
         )
     }
 }
