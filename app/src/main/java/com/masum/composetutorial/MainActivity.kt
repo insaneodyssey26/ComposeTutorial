79a9a379955f2803951651f9170d902058fec515
Masum <hydrapikachu29@gmail.com>
2025-05-31 01:32:43 +0530

    Implemented all 3 texts

--- app/src/main/java/com/masum/composetutorial/MainActivity.kt
+++ app/src/main/java/com/masum/composetutorial/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.Nullable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,6 +17,8 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.masum.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,6 +49,23 @@ fun Article(
     bgimage: Painter,
     modifier: Modifier = Modifier,
 ) {
-
+    Column (modifier = Modifier){
+        Image(painter = bgimage,  contentDescription = null)
+        Text(
+            text = title,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = subheading,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = content,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+    }
 }
 
