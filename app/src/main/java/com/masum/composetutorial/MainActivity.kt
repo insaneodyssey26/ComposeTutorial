f981af069371f67fe60fe745c011916af3340fe8
Masum <hydrapikachu29@gmail.com>
2025-05-30 16:52:33 +0530

    Converted texts to string resource

--- app/src/main/java/com/masum/composetutorial/MainActivity.kt
+++ app/src/main/java/com/masum/composetutorial/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.masum.composetutorial.ui.theme.ComposeTutorialTheme
 
@@ -25,7 +26,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TutorialApp() {
-    Text(text = "Jetpack Compose Tutorial")
-    Text(text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.")
-    Text(text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.")
+    Text(text = stringResource(R.string.jetpack_compose_tutorial_title))
+    Text(text = stringResource(R.string.jetpack_compose_tutorial_subtitle))
+    Text(text = stringResource(R.string.jetpack_compose_tutorial_body))
 }
